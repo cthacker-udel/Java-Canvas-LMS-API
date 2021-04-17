06b3f271d5be6224e958721da39373565112c7e7
cthacker-udel <cthacker@udel.edu>
2021-04-17 11:20:12 -0400

    Added : Tests: Index of Active Global Notification

---
+++ src/test/java/getRequests/AccountNotificationTest.java
@@ -0,0 +1,34 @@
+package getRequests;
+
+import Client.CanvasClient;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class AccountNotificationTest {
+
+    static CanvasClient client;
+
+    static{
+
+        client = new CanvasClient("","");
+        client.setToken("//");
+
+
+    }
+
+    @Test
+    void testIndexOfActiveGlobalNotification() throws IOException {
+
+        AccountNotification accountNotification = client.getAccountNotification();
+
+        accountNotification.setAccountId(112);
+        accountNotification.setNotificationId(3332);
+
+        assertNotNull(client.indexOfActiveGlobalNotifactionCurrUser(client));
+
+    }
+
+}
