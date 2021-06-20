1dba7203e97dab21177f5a7c8a601b0931af6236
cthacker-udel <cthacker@udel.edu>
2021-06-19 23:49:23 -0400

    Added proper REST call in appointmentGroupsInterface.java

--- src/main/java/Model/appointmentGroupsInterface.java
+++ src/main/java/Model/appointmentGroupsInterface.java
@@ -2,9 +2,7 @@ package Model;
 
 import Controller.AppointmentGroupsController.AppointmentGroup.AppointmentGroup;
 import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Header;
-import retrofit2.http.QueryMap;
+import retrofit2.http.*;
 
 import java.util.List;
 import java.util.Map;
@@ -13,5 +11,8 @@ public interface appointmentGroupsInterface {
 
     @GET("https://udel.instructure.com/api/v1/appointment_groups")
     Call<List<AppointmentGroup>> getAppointmentGroupList(@QueryMap Map<String,Object> queries, @Header("Authorization") String token);
+
+    @POST("https://udel.instructure.com/api/v1/appointment_groups")
+    Call<Void> createAppointmentGroups(@Body Map<String,Object>  body, @Header("Authorization") String auth);
 
 }
