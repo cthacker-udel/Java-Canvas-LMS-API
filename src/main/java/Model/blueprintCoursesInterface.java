f88473dbdbfb77daea2d11e670a2f55e1b29c6f8
cthacker-udel <cthacker@udel.edu>
2021-07-01 17:07:04 -0400

    Added : Blueprint Courses API : Begin migration interface header

---
+++ src/main/java/Model/blueprintCoursesInterface.java
@@ -0,0 +1,26 @@
+package Model;
+
+import Controller.BlueprintCourseController.BlueprintMigration;
+import Controller.BlueprintCourseController.BlueprintTemplate;
+import Controller.CourseController.Course;
+import retrofit2.Call;
+import retrofit2.http.*;
+
+import java.util.List;
+import java.util.Map;
+
+public interface blueprintCoursesInterface {
+
+    @GET("https://udel.instructure.com/api/v1/courses/{courseId}/blueprint_templates/{templateId}")
+    Call<BlueprintTemplate> getBlueprintInformation(@Path("courseId") String courseId, @Path("templateId") String templateId, @Header("Authorization") String auth);
+
+    @GET("https://udel.instructure.com/api/v1/courses/{courseId}/blueprint_templates/{templateId}/associated_courses")
+    Call<List<Course>> getAssociatedCourseInformation(@Path("courseId") String courseId, @Path("templateId") String templateId, @Header("Authorization") String auth);
+
+    @PUT("https://udel.instructure.com/api/v1/courses/{courseId}/blueprint_templates/{templateId}/update_associations")
+    Call<Void> updateAssociatedCourses(@Path("courseId") String courseId, @Path("templateId") String templateId, @Header("Authorization") String auth, @Body Map<String,Object> body);
+
+    @POST("https://udel.instructure.com/api/v1/courses/{courseId}/blueprint_templates/{templateId}/migrations")
+    Call<BlueprintMigration> beginMigration(@Path("courseId") String courseId, @Path("templateId") String templateId, @Header("Authorization") String auth, @Body Map<String,Object> body);
+
+}
