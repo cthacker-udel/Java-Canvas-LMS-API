f18208e4ffee2b649c268eccc7c4f3234445b2fa
cthacker-udel <cthacker@udel.edu>
2021-07-11 21:41:00 -0400

    Added : ContentMigrations API : added rest api calls to list content migrations

--- src/main/java/Model/contentMigrationsInterface.java
+++ src/main/java/Model/contentMigrationsInterface.java
@@ -3,12 +3,10 @@ package Model;
 import Controller.ContentMigrationsController.ContentMigration;
 import Controller.ContentMigrationsController.MigrationIssue;
 import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Header;
-import retrofit2.http.PUT;
-import retrofit2.http.Path;
+import retrofit2.http.*;
 
 import java.util.List;
+import java.util.Map;
 
 public interface contentMigrationsInterface {
 
@@ -36,16 +34,33 @@ public interface contentMigrationsInterface {
     @GET("https://udel.instructure.com/api/v1/users/{userId}/content_migrations/{contentMigrationId}/migration_issues/{migrationIssueId}")
     Call<MigrationIssue> getMigrationIssueUserId(@Path("userId") String userId, @Path("contentMigrationId") String contentMigrationId, @Path("migrationIssueId") String migrationIssueId);
 
+
+
     @PUT("https://udel.instructure.com/api/v1/accounts/{accountId}/content_migrations/{contentMigrationId}/migration_issues/{migrationIssueId}")
-    Call<MigrationIssue> updateMigrationIssueAccountId(@Path("accountId") String accountId, @Path("contentMigrationId") String contentMigrationId, @Path("migrationIssueId") String migrationIssueId, @Header("Authorization") String auth);
+    Call<MigrationIssue> updateMigrationIssueAccountId(@Path("accountId") String accountId, @Path("contentMigrationId") String contentMigrationId, @Path("migrationIssueId") String migrationIssueId, @Header("Authorization") String auth, @QueryMap Map<String,Object> queries);
 
     @PUT("https://udel.instructure.com/api/v1/courses/{courseId}/content_migrations/{contentMigrationId}/migration_issues/{migrationIssueId}")
-    Call<MigrationIssue> updateMigrationIssueCourseId(@Path("courseId") String courseId, @Path("contentMigrationId") String contentMigrationId, @Path("migrationIssueId") String migrationIssueId, @Header("Authorization") String auth);
+    Call<MigrationIssue> updateMigrationIssueCourseId(@Path("courseId") String courseId, @Path("contentMigrationId") String contentMigrationId, @Path("migrationIssueId") String migrationIssueId, @Header("Authorization") String auth, @QueryMap Map<String,Object> queries);
 
     @PUT("https://udel.instructure.com/api/v1/groups/{groupId}/content_migrations/{contentMigrationId}/migration_issues/{migrationIssueId}")
-    Call<MigrationIssue> updateMigrationIssueGroupId(@Path("groupId") String groupId, @Path("contentMigrationId") String contentMigrationId, @Path("migrationIssueId") String migrationIssueId, @Header("Authorization") String auth);
+    Call<MigrationIssue> updateMigrationIssueGroupId(@Path("groupId") String groupId, @Path("contentMigrationId") String contentMigrationId, @Path("migrationIssueId") String migrationIssueId, @Header("Authorization") String auth, @QueryMap Map<String,Object> queries);
 
     @PUT("https://udel.instructure.com/api/v1/users/{userId}/content_migrations/{contentMigrationId}/migration_issues/{migrationIssueId}")
-    Call<MigrationIssue> updateMigrationIssueUserId(@Path("userId") String userId, @Path("contentMigrationId") String contentMigrationId, @Path("migrationIssueId") String migrationIssueId, @Header("Authorization") String auth);
+    Call<MigrationIssue> updateMigrationIssueUserId(@Path("userId") String userId, @Path("contentMigrationId") String contentMigrationId, @Path("migrationIssueId") String migrationIssueId, @Header("Authorization") String auth, @QueryMap Map<String,Object> queries);
+
+
+
+
+    @GET("https://udel.instructure.com/api/v1/accounts/{accountId}/content_migrations")
+    Call<List<ContentMigration>> listContentMigrationsAccountId(@Path("accountId") String accountId, @Header("Authorization") String auth);
+
+    @GET("https://udel.instructure.com/api/v1/courses/{courseId}/content_migrations")
+    Call<List<ContentMigration>> listContentMigrationsCourseId(@Path("courseId") String courseId, @Header("Authorization") String auth);
+
+    @GET("https://udel.instructure.com/api/v1/groups/{groupId}/content_migrations")
+    Call<List<ContentMigration>> listContentMigrationsGroupId(@Path("groupId") String groupId, @Header("Authorization") String auth);
+
+    @GET("https://udel.instructure.com/api/v1/users/{userId}/content_migrations")
+    Call<List<ContentMigration>> listContentMigrationsUserId(@Path("userId") String userId, @Header("Authorization") String auth);
 
 }
