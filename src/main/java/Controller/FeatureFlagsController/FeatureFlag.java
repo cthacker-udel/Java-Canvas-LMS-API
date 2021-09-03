44793490449ceb0e928ab01c3e3663d3c9c30513
cthacker-udel <cthacker@udel.edu>
2021-09-03 07:42:53 -0400

    Added : Feature API : Update Feature Controller

--- src/main/java/Controller/FeatureFlagsController/FeatureFlag.java
+++ src/main/java/Controller/FeatureFlagsController/FeatureFlag.java
@@ -5,12 +5,37 @@ import com.google.gson.annotations.SerializedName;
 
 public class FeatureFlag {
 
+    @SerializedName("context_type")
+    @Expose
+    private String contextType;
+    @SerializedName("context_id")
+    @Expose
+    private Integer contextId;
     @SerializedName("feature")
     @Expose
     private String feature;
     @SerializedName("state")
     @Expose
     private String state;
+    @SerializedName("locked")
+    @Expose
+    private Boolean locked;
+
+    public String getContextType() {
+        return contextType;
+    }
+
+    public void setContextType(String contextType) {
+        this.contextType = contextType;
+    }
+
+    public Integer getContextId() {
+        return contextId;
+    }
+
+    public void setContextId(Integer contextId) {
+        this.contextId = contextId;
+    }
 
     public String getFeature() {
         return feature;
@@ -26,6 +51,14 @@ public class FeatureFlag {
 
     public void setState(String state) {
         this.state = state;
+    }
+
+    public Boolean getLocked() {
+        return locked;
+    }
+
+    public void setLocked(Boolean locked) {
+        this.locked = locked;
     }
 
 }
--- src/main/java/getRequests/FeatureFlag.java
+++ src/main/java/getRequests/FeatureFlag.java
@@ -1,65 +1,8 @@
 package getRequests;
 
 import Client.CanvasClient;
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
 
 public class FeatureFlag extends CanvasClient {
 
-    @SerializedName("context_type")
-    @Expose
-    private String contextType;
-    @SerializedName("context_id")
-    @Expose
-    private Integer contextId;
-    @SerializedName("feature")
-    @Expose
-    private String feature;
-    @SerializedName("state")
-    @Expose
-    private String state;
-    @SerializedName("locked")
-    @Expose
-    private Boolean locked;
-
-    public String getContextType() {
-        return contextType;
-    }
-
-    public void setContextType(String contextType) {
-        this.contextType = contextType;
-    }
-
-    public Integer getContextId() {
-        return contextId;
-    }
-
-    public void setContextId(Integer contextId) {
-        this.contextId = contextId;
-    }
-
-    public String getFeature() {
-        return feature;
-    }
-
-    public void setFeature(String feature) {
-        this.feature = feature;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public Boolean getLocked() {
-        return locked;
-    }
-
-    public void setLocked(Boolean locked) {
-        this.locked = locked;
-    }
 
 }
